ec581b7bb22ed050f4e774374370fcc966c8860d
jsanc525 <45319394+jsanc525@users.noreply.github.com>
2021-03-19 18:58:34 -0400

    adding pc skills dict

---
+++ skills.py
@@ -0,0 +1,10 @@
+skills = {'acrobatics':{'ability':'dex', 'prof':False}, 'animal handling':{'ability':'wis', 'prof': False},
+         'arcana':{'ability': 'int', 'prof': False}, 'athletics':{'ability':'str', 'prof': False}, 
+         'deception':{'ability':'cha', 'prof': False}, 'history':{'ability': 'int', 'prof': False}, 
+         'insight':{'ability':'wis', 'prof': False}, 'intimidation':{'ability':'cha', 'prof': False},
+         'intimidation':{'ability':'cha', 'prof':'false'}, 'investigation':{'ability':'int', 'prof': False},
+         'medicine':{'ability':'wis', 'prof': False}, 'nature':{'ability':'int', 'prof': False},
+         'perception':{'ability':'wis', 'prof': False}, 'performance':{'ability':'cha', 'prof': False},
+         'persuasion':{'ability':'cha', 'prof': False}, 'religion':{'ability':'int', 'prof': False},
+         'sleight of hand':{'ability': 'dex', 'prof': False}, 'stealth':{'ability': 'dex', 'prof': False},
+         'survival':{'ability':'wis', 'prof': False}}
