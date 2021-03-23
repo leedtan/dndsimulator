a7fc46545ba30dff4a02b2c9dfd8181e06eec60b
jsanc525 <45319394+jsanc525@users.noreply.github.com>
2021-03-23 18:39:16 -0400

    minor change to creature key naming

--- creatures.py
+++ creatures.py
@@ -1,4 +1,4 @@
-aarakocra = {'name':'aarakocra', 'abilities':{'str':10, 'dex':14, 'con':10, 'int':11, 'wis': 12, 'cha':11},'hp':13, 'alt_hp':[8, 3], 'ac': 12, 
+aarakocra = {'s_name':'aarakocra', 'abilities':{'str':10, 'dex':14, 'con':10, 'int':11, 'wis': 12, 'cha':11},'hp':13, 'alt_hp':[8, 3], 'ac': 12, 
         'actions':['attack', 'cast', 'dash', 'disengage', 'dodge', 'help', 'hide','ready', 'search', 'use an object', 
                    'improvise'],
         'bonus_actions': {}, 'skills': None, 'action_count': 0, 'b_action_count': 0, 'reaction_count': 0, 'feats':{},
