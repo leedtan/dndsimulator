d0c7f2c29cfc87b82932995b231ac442b29b6b37
jsanc525 <45319394+jsanc525@users.noreply.github.com>
2021-03-31 23:31:37 -0400

    phb armor dicts complete

---
+++ armor.py
@@ -0,0 +1,95 @@
+##### light armor
+padded = {
+    'type': 'light',
+    'cost': 5,
+    'armor_class':{'base': 11, 'dex_mod': True, 'dex_mod_max': False},
+    'str_req': False,
+    'stealth_penalty': True,
+    'weight': 8}
+leather = {
+    'type': 'light',
+    'cost': 10,
+    'armor_class':{'base': False, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 10}
+studded_leather = {
+    'type': 'light',
+    'cost': 45,
+    'armor_class':{'base': 12, 'dex_mod': True , 'dex_mod_max': False},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 13}
+#### medium armor
+hide = {
+    'type': 'medium',
+    'cost': 10,
+    'armor_class':{'base': 12, 'dex_mod': True, 'dex_mod_max': 2},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 12}
+chain_shirt = {
+    'type': 'medium',
+    'cost': 50,
+    'armor_class':{'base': 13, 'dex_mod': True, 'dex_mod_max': 2},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 20}
+scale_mail = {
+    'type': 'medium',
+    'cost': 50,
+    'armor_class':{'base': 14, 'dex_mod': True, 'dex_mod_max': 2},
+    'str_req': False,
+    'stealth_penalty': True,
+    'weight': 45}
+breast_plate = {
+    'type': 'medium',
+    'cost': 400,
+    'armor_class':{'base': 14, 'dex_mod': True, 'dex_mod_max': 2},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 20}
+half_plate = {
+    'type': 'medium',
+    'cost': 750,
+    'armor_class':{'base': 15, 'dex_mod': True, 'dex_mod_max': 2},
+    'str_req': False,
+    'stealth_penalty': True,
+    'weight': 40}
+    #### heavy armor
+    ring_mail = {
+    'type': 'heavy',
+    'cost': 30,
+    'armor_class':{'base': 14, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': False,
+    'stealth_penalty': True,
+    'weight': 40}
+chain_mail = {
+    'type': 'heavy',
+    'cost': 75,
+    'armor_class':{'base': 16, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': 13,
+    'stealth_penalty': True,
+    'weight': 55}
+splint = {
+    'type': 'heavy',
+    'cost': 200,
+    'armor_class':{'base': 17, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': 15,
+    'stealth_penalty': True,
+    'weight': 60}
+plate = {
+    'type': 'heavy',
+    'cost': 1500,
+    'armor_class':{'base': 18, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': 15,
+    'stealth_penalty': True,
+    'weight': 65}
+### shield
+shield = {
+    'type': 'shield',
+    'cost': 10,
+    'armor_class':{'base': 10, 'dex_mod': False, 'dex_mod_max': False},
+    'str_req': False,
+    'stealth_penalty': False,
+    'weight': 6}
