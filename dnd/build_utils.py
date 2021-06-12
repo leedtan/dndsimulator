82467ee2a406b06735ec13b8ff10ddb7e1367d57
lee <leedtan@gmail.com>
2021-06-12 14:07:32 -0400

    implemented spell slots

--- dnd/build_utils.py
+++ dnd/build_utils.py
@@ -27,10 +27,32 @@ def calc_spell_slots(class_levels):
             spell_level += level
         if clss in ["pal", "rng"]:
             spell_level += level // 2
-    spell_slots = [0 for _ in range(9)]]
+
+    spell_slots = [0 for _ in range(9)]
+    spell_slots[0] = min((spell_level >= 1) * (1 + spell_level), 4)
+    spell_slots[1] = min((spell_level >= 3) * (spell_level - 1), 3)
+    spell_slots[2] = min((spell_level >= 5) * (spell_level - 3), 3)
+    spell_slots[3] = min((spell_level >= 7) * (spell_level - 6), 3)
+    spell_slots[4] = min((spell_level >= 9) * (spell_level - 8), 2) + (spell_level >= 18)
+    spell_slots[5] = (spell_level >= 11) + (spell_level >= 19)
+    spell_slots[6] = (spell_level >= 13) + (spell_level >= 20)
+    spell_slots[8] = spell_level >= 15
+    spell_slots[9] = spell_level >= 17
+    return spell_slots
+
+
+def calc_pact_slots(class_levels):
+    spell_level = class_levels["warlock"]
+    spell_slots = [0 for _ in range(5)]
+    if spell_level > 0:
+        pact_level = min((spell_level + 1) // 2, 5)
+        num_spell_slots = 1 + (spell_level >= 2) + (spell_level >= 11) + (spell_level >= 17)
+        spell_slots[pact_level] = num_spell_slots
+    return spell_slots
+
 
 class CharacterProgression:
-    def __init__(starting_stats, classes, weapon, armor, feats, hasshield=False):
+    def __init__(self, starting_stats, classes, weapon, armor, feats, hasshield=False):
         self.attacks = 1
         self.stats = self.starting_stats = starting_stats
         self.classes = classes
@@ -50,15 +72,16 @@ class CharacterProgression:
         if self.hasshield:
             self.ac += 2
 
-    def sample_at_level(level):
+    def sample_at_level(self, level):
         for lvl in range(level):
             self.apply_level(lvl)
         self.modifiers = calc_modifiers(self.stats)
         self.max_hps += level * self.modifiers["con"]
         self.spell_slots = self.max_spell_slots = calc_spell_slots(self.class_levels)
+        self.pact_slots = self.max_pact_slots = calc_pact_slots(self.class_levels)
         self.ac = self.calc_ac()
 
-    def apply_feats(feats):
+    def apply_feats(self, feats):
         for feat in self.feats:
             if isinstance(feat, ASI):
                 for stat in feat.stats:
--- dnd/built_characters.py
+++ dnd/built_characters.py
@@ -1,26 +1,28 @@
-from build_features import agonizing, repelling
+# from build_features import agonizing, repelling
 
 from attacks import MeleeMultiAttack  # noqa
-from attacks import (Attack, Damage, MeleeAttack, MultiAttack, RangedAttack,
-                     RangedMultiAttack)
-from build_utils import ASI, CharacterProgression, Feat, Spell
-from character import Character
-from effects import BlastBack
-from spells import HealingWord, MagicMissile
-# from strategies import Character, Defender, PreserveLife
-from strategies import PreserveLife
 
-hexadin8 = CharacterProgression(
+# from attacks import Attack, Damage, MeleeAttack, MultiAttack, RangedAttack, RangedMultiAttack
+from build_utils import CharacterProgression, Feat, Spell  # , ASI
+
+# from character import Character
+# from effects import BlastBack
+# from spells import HealingWord, MagicMissile
+
+# # from strategies import Character, Defender, PreserveLife
+# from strategies import PreserveLife
+
+hexadin = CharacterProgression(
     starting_stats={"str": 16, "dex": 8, "con": 15, "int": 8, "wis": 8, "cha": 16},
     classes=["pal"] * 6 + ["warlock"] * 5 + ["pal"] * 12,
     weapon="glaive",
-    armor = 'scalemail',
-    feats=[Feat(["pam"]), None, None, Feat(["cha", "cha"]), None, None,]
+    armor="scalemail",
+    feats=[Feat(["pam"]), None, None, Feat(["cha", "cha"]), None, None]
     # Warlock
-    + [Feat([Spell("eb"), Spell('hex')]), Feat(["agonize"]), Feat(["pactblade", "pactweapon", Spell('mirrorimage')]),]
-    + [Feat(["warcaster"]), Feat([Spell("spiritshroud")]),]
+    + [Feat([Spell("eb"), Spell("hex")]), Feat(["agonize"]), Feat(["pactblade", "pactweapon", Spell("mirrorimage")])]
+    + [Feat(["warcaster"]), Feat([Spell("spiritshroud")])]
     # Paladin
-    + [None, Feat(["resilientcon"]),] + [None, None, None),],
+    + [None, Feat(["resilientcon"])] + [None, None, None],
 )
 
 # starting_stats={
